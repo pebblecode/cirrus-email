0c2ffc472d8808634cfc911eda3d3a8325204abd
Dave Hillier <dave.hillier1@gmail.com>
2016-05-13 12:49:51 +0100

    simplified config

--- cirrus/email.py
+++ cirrus/email.py
@@ -3,15 +3,11 @@ import boto.ses
 from flask import current_app
 
 
-def send_email(to_email_addresses, email_body, subject, from_email, from_name, tags, reply_to=None):
+def send_email(to_email_addresses, email_body, subject, from_email, from_name, tags, reply_to=None, region='eu-west-1'):
     if isinstance(to_email_addresses, string_types):
         to_email_addresses = [to_email_addresses]
 
-    # TODO: this seems strange to have to pass credentials from here.
-    conn = boto.ses.connect_to_region(
-        region=current_app.config['AWS_DEFAULT_REGION'],
-        aws_access_key_id=current_app.config['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY'])
+    conn = boto.ses.connect_to_region(region=region)
     
     result = conn.send_email(
         source="%s <%s>" % (from_name, from_email),

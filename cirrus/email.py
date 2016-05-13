e0862aad57e127a34cdbf057a371a23acf5e22e2
Dave Hillier <dave.hillier1@gmail.com>
2016-05-13 15:37:52 +0100

    fixed imports

--- cirrus/email.py
+++ cirrus/email.py
@@ -1,19 +1,19 @@
 import boto.ses
 import six
 
-from flask import current_app
-
+from flask import current_app, flash
+from flask._compat import string_types
 
 def send_email(to_email_addresses, email_body, subject, from_email, from_name, tags, reply_to=None, region='eu-west-1'):
     if isinstance(to_email_addresses, string_types):
         to_email_addresses = [to_email_addresses]
 
-    conn = boto.ses.connect_to_region(region=region)
+    conn = boto.ses.connect_to_region(region)
     
     result = conn.send_email(
         source="%s <%s>" % (from_name, from_email),
         subject=subject,
-        html_body=html,
+        body=email_body,
         to_addresses=to_email_addresses,
         format='html')
 

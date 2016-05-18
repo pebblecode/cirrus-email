6c162089b4a70efdea3e7cc2c8301e46fabf1d55
Dave Hillier <dave.hillier1@gmail.com>
2016-05-18 12:03:38 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import pip.download
 
 try:
     long_description = open("README.rst").read()
 except IOError:
     long_description = ""
 
+requirements = list(parse_requirements('requirements.txt',
+                                       session=pip.download.PipSession()))
+
+install_requires = [str(r.req) for r in requirements]
+
 setup(
     name="cirrus-email",
-    version="0.0.2",
+    version="0.0.3",
     description="AWS SES Email helpers for Project Cirrus",
     license="MIT",
     author="pebble {code}",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=install_requires,
     long_description=long_description,
     classifiers=[
         "Programming Language :: Python",
